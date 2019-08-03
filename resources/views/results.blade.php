bdb74704c06e61ea19a8bc660148dca11f5aca6b
Bikash Gurung <gurungbikash145@gmail.com>
2019-08-03 19:04:46 +0545

    new way of scraping

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -21,57 +21,48 @@
     </div>
 
 
-<div class="container-fluid">
-    @if(!empty($results))
-    <?php foreach($results as $result): ?>
-
-        <div class="row productBox">
-        <div class="col-sm-2 box">
-                <div class="inner"><img src="<?php echo $result->link; ?>" alt=""></div>
-        </div>
-        <div class="col-sm-7 box">
-                <div class="inner">
-                    <div class="row centerMainRow">
-                        <p><?php echo $result->title; ?></p>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-3"><?php 
-                            if($result->site == "daraz") {
-                                echo "<img src='https://laz-img-cdn.alicdn.com/images/ims-web/TB1eIwbmljTBKNjSZFuXXb0HFXa.png'>";
-                            } else if($result->site == "muncha") {
-                                echo "<img src='https://muncha.com/assets/images/logo.gif'>";
-                            }
-                        ?></div>
-                    </div>
-                    <div class="row centerMainRow">
-                        <img src="<?php echo $result->image ? $result->image : 'http://127.0.0.1:8000/images/no_image.jpg'; ?>" alt="#">
+<section class="section--product-results">
+    <div class="container">
+        @if(!empty($results))
+        <?php foreach($results as $result): ?>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="product--image">
+                        <img class="img-fluid" src="<?php echo $result->image; ?>" alt="">
                     </div>
                 </div>
-        </div>
-            <div class="col-sm-3 box">
-                    <div class="inner">
-                        <div class="row centerMainRow">
-                            <p>Rs. <?php echo $result->price; ?></p>
+
+                <div class="col-md-6">
+                    <div class="product--detail">
+                        <h3 class="product--title"><?php echo $result->title; ?></h3>
+
+                        <div class="site--logo">
+                            <img src="{{ $result->site }}" alt="" class="img-fluid">
                         </div>
-                        <div class="row">
-                            <a target="_blank" href="<?php echo $result->link; ?>" class="btn btn-primary">View Deal</a>
-                        </div>
-                    </div> 
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="product--specific-detail">
+                        <div class="product-price">Rs. <?php echo $result->price; ?></div>
+
+                        <a target="_blank" href="<?php echo $result->link; ?>" class="btn btn-primary">View Deal</a>
+                    </div>
+                </div>
             </div>
-        </div>
 
-    <?php endforeach; ?>  
-    @else
+        <?php endforeach; ?> 
+        
+        @else
 
-    <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div class="text-danger">Something went wrong. Please try again.</div>
             </div>
         </div>
-    </div>
 
-    @endif
-</div>
+        @endif
+    </div>
+</section>
 
 @endsection
